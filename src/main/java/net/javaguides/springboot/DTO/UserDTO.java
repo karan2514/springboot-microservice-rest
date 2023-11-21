41e44eb7bc5cdc5749afe8adfa08bb61f200ff59
karan2514 <karan2514singh@gmail.com>
2023-11-21 16:38:46 +0530

    API documentation using Swagger and annotation support

--- src/main/java/net/javaguides/springboot/DTO/UserDTO.java
+++ src/main/java/net/javaguides/springboot/DTO/UserDTO.java
@@ -1,11 +1,17 @@
 package net.javaguides.springboot.DTO;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "UserDTO Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,10 +19,20 @@ import lombok.Setter;
 public class UserDTO {
 
     private Long id;
-
+    @Schema(
+            description = "User First Name"
+    )
+    @NotEmpty
     private String firstName;
-
+    @Schema(
+            description = "User Last Name"
+    )
+    @NotEmpty
     private String lastName;
-
+    @Schema(
+            description = "User Email Address"
+    )
+    @NotEmpty
+    @Email
     private String email;
 }
--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,19 +1,26 @@
 package net.javaguides.springboot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.DTO.UserDTO;
 
-import net.javaguides.springboot.exception.ErrorDetails;
-import net.javaguides.springboot.exception.ResourceNotFoundException;
+
 import net.javaguides.springboot.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
+
+
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST API for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User,Get All Users, Delete User"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -21,12 +28,28 @@ public class UserController {
 
     private UserService userService;
 
+    @Operation(
+            summary = "Create User REST API",
+            description = "Create user REST API is used to save user in database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user) {
+    public ResponseEntity<UserDTO> createUser(@RequestBody @Valid UserDTO user) {
       UserDTO savedUser =  userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get User By ID REST API",
+            description = "Get user REST API is used to get the user using Id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Long userId){
 
@@ -34,18 +57,42 @@ public class UserController {
         return  new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get All Users REST API",
+            description = "Get all the users REST API is used to retrieve all the users"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 201 success"
+    )
     @GetMapping("/AllUsers")
     public ResponseEntity<List<UserDTO>> getAllUsers(){
         List<UserDTO> users = userService.getAllUsers();
         return  new ResponseEntity<>(users,HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update User REST API",
+            description = "Update user REST API is used to update an existing user"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 201 SUCCESS"
+    )
     @PutMapping("{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Long userid,@RequestBody UserDTO user){
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Long userid,@RequestBody @Valid UserDTO user){
         user.setId(userid);
         UserDTO updatedUser = userService.updateUser(user);
         return  new ResponseEntity<>(updatedUser,HttpStatus.OK);
     }
+    @Operation(
+            summary = "Delete User REST API",
+            description = "Delete user REST API is used to delete a user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 201 SUCCESS"
+    )
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
